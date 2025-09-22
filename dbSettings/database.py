c730f227b14b7c7f8e33956a0779f12e02251d8d
gabitenoriob <gabitenoriob@gmail.com>
2025-09-22 12:16:42 -0300

    fix import db

--- dbSettings/database.py
+++ dbSettings/database.py
@@ -1,4 +1,6 @@
 import os
 from flask_sqlalchemy import SQLAlchemy
-
+import logging
+logging.basicConfig()
+logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 db = SQLAlchemy()
--- dbSettings/reuniao_schema.py
+++ dbSettings/reuniao_schema.py
@@ -2,5 +2,5 @@ from .database import db
 
 class Reuniao(db.Model):
     id = db.Column(db.String(12), primary_key=True)
-    descricao = db.Column(db.String(200), nullable=False)
+    descricao = db.Column(db.String(255), nullable=False)
     data_criacao = db.Column(db.String(200), nullable=False)
