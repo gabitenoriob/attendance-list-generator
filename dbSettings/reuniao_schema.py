3e71769a88bb125ec85bbb5900a6904e43747172
gabitenoriob <gabitenoriob@gmail.com>
2025-09-22 12:11:47 -0300

    fix orm

--- dbSettings/reuniao_schema.py
+++ dbSettings/reuniao_schema.py
@@ -1,6 +1,6 @@
 from .database import db 
 
 class Reuniao(db.Model):
-    id = db.Column(db.String(8), primary_key=True)
+    id = db.Column(db.String(12), primary_key=True)
     descricao = db.Column(db.String(200), nullable=False)
-    data_criacao = db.Column(db.String(20), nullable=False)
+    data_criacao = db.Column(db.String(200), nullable=False)
